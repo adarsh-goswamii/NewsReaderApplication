ef6d578811d0eb918a3ab53aba74827213d689cb
Adarsh Goswami <adarsh010120@gmail.com>
2020-10-22 15:31:08 +0530

    parsed xml file

--- app/src/main/java/com/example/newsreader/MainActivity.java
+++ app/src/main/java/com/example/newsreader/MainActivity.java
@@ -15,11 +15,13 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG="News Reader";
     private RecyclerView recyclerView;
+    private ArrayList<NewsItem> news;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,14 +43,14 @@ public class MainActivity extends AppCompatActivity {
             {
                 try {
                     initXMLPullParser(inputStream);
-                } catch (XmlPullParserException e) {
+                } catch (XmlPullParserException | IOException e) {
                     e.printStackTrace();
                 }
             }
             return null;
         }
 
-        private void initXMLPullParser(InputStream inputStream) throws XmlPullParserException {
+        private void initXMLPullParser(InputStream inputStream) throws XmlPullParserException, IOException {
             XmlPullParser parser= Xml.newPullParser();
             parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
             parser.setInput(inputStream, null);
@@ -60,6 +62,118 @@ public class MainActivity extends AppCompatActivity {
              * FEATURE_PROCESS_NAMESPACE : is a process of differentiating different tags with same name but here we dont want to make
              * the code complicated so we have passed false for the feature process namespace.
              */
+
+            parser.next();
+            // parser.next() return an integer which tells what we have an opening tag, maybe end tag or a text.
+            parser.require(XmlPullParser.START_TAG, null, "rss");
+            // we are going to keep on parsing the xml file until we get an end tag for rss.
+            while(parser.next()!= XmlPullParser.END_TAG)
+            {
+                if(parser.getEventType()!= XmlPullParser.START_TAG) // skip any metadata that we may get till we see an starting tag
+                    continue;
+
+                parser.require(XmlPullParser.START_TAG, null, "channel");
+                while(parser.next()!= XmlPullParser.END_TAG) // till the tag channel ends check code beautify website and paste the link of rss.
+                {
+                    if(parser.getEventType()!= XmlPullParser.START_TAG)
+                        continue;
+
+                    String tag= parser.getName();
+                    if(tag.equals("item"))
+                    {
+                        parser.require(XmlPullParser.START_TAG, null, "item");
+                        String title="", link="", des="", date="";
+                        while(parser.next()!= XmlPullParser.END_TAG) // we will parse through our item and get all the details which we want.
+                        {
+                            if(parser.getEventType()!= XmlPullParser.START_TAG)
+                                continue;
+
+                            String tagName= parser.getName();
+                            if(tagName.equals("title"))
+                            {
+                                title= getContent(parser, "title");
+                            }
+                            else if(tagName.equals("link"))
+                            {
+                                link= getContent(parser, "link");
+                            }
+                            else if(tagName.equals("description"))
+                            {
+                                des= getContent(parser, "description");
+                            }
+                            else if(tagName.equals("pubdate"))
+                            {
+                                date= getContent(parser, "pubdate");
+                            }
+                            else
+                            {
+                                skipTag(parser);
+                            }
+                        }
+                        NewsItem obj= new NewsItem(title, des, link, date);
+                        news.add(obj);
+                    }
+                    else
+                        skipTag(parser);
+                }
+            }
+        }
+
+        private String getContent(XmlPullParser parser, String title) throws IOException, XmlPullParserException {
+            parser.require(XmlPullParser.START_TAG, null, title);
+
+            String content="";
+            if(parser.next()== XmlPullParser.TEXT)
+            {
+                content= parser.getText();
+                parser.next();
+            }
+
+            return content;
+        }
+
+        private void skipTag(XmlPullParser parser) throws XmlPullParserException, IOException {
+            if(parser.getEventType()!= XmlPullParser.START_TAG)
+                throw new IllegalStateException();
+            else
+            {
+                /**
+                 * basically this part of the code is where we are skipping the irrelevant tags.
+                 * we have initially given number value 1 as we know we have are under a starting tag
+                 *
+                 * (i). what we are doing is when we get a start tag we increase the value
+                 * (ii). when we get an closing tag we decrease the value of number.
+                 *
+                 * Think of it like this we can have a nested tag so all the tags starting inside the tag which we wanna skip will eventually
+                 * get neglected and the value only goes to zero when we receive the tags which we want.
+                 *
+                 * <tag> initially value is 1
+                 *     <inner tag> number++ as opening tag =2
+                 *         </inner> number-- =1
+                 *      <inner tag> =2
+                 *          </inner>  =1
+                 * </end tag> =0
+                 */
+
+
+                int number= 1;
+                while(number!=0)
+                {
+                    switch(parser.next())
+                    {
+                        case XmlPullParser.START_TAG: {
+                            number++;
+                            break;
+                        }
+                        case XmlPullParser.END_TAG: {
+                            number--;
+                            break;
+                        }
+                        default:
+                            break;
+                    }
+                }
+            }
         }
 
         private InputStream getInputStream()
@@ -79,7 +193,5 @@ public class MainActivity extends AppCompatActivity {
 
             return null;
         }
-
-
     }
 }
